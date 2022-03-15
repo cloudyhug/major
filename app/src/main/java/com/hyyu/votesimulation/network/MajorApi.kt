fb2e60e253e2899c093dadcbed220baa7f5ac72a
antoinedanibo <danibo.a@sfeir.com>
2022-03-15 14:17:02 +0100

    Changed the request implementation for /register request to accept just a body instead of a form

--- app/src/main/java/com/hyyu/votesimulation/network/MajorApi.kt
+++ app/src/main/java/com/hyyu/votesimulation/network/MajorApi.kt
@@ -1,24 +1,20 @@
 package com.hyyu.votesimulation.network
 
-import com.hyyu.votesimulation.network.response.BlogObjectResponse
+import com.hyyu.votesimulation.network.body.CredentialsObjectBody
 import com.hyyu.votesimulation.network.response.ConnectionObjectResponse
-import com.hyyu.votesimulation.network.response.EmptyObjectResponse
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
-import retrofit2.http.POST
+import com.hyyu.votesimulation.network.response.RegisterObjectResponse
+import retrofit2.http.*
 
 interface MajorApi {
 
     companion object Road {
         //const val BASE_URL = "https://open-api.xyz/placeholder/"
-        const val BASE_URL = "http:/192.168.1.24:8080/"
+        const val BASE_URL = "http:/10.2.32.52:8080/"
 
         const val CONNECT = "connect/"
         const val REGISTER = "register/"
     }
 
-    @FormUrlEncoded
     @POST(CONNECT)
     suspend fun connect(
         @Field("login") login: String,
@@ -26,12 +22,9 @@ interface MajorApi {
         @Field("clientId") deviceName: String
     ): ConnectionObjectResponse
 
-    @FormUrlEncoded
     @POST(REGISTER)
     suspend fun register(
-        @Field("login") login: String,
-        @Field("password") password: String,
-        @Field("clientId") deviceName: String
-    ): EmptyObjectResponse
+        @Body credentials: CredentialsObjectBody
+    ): RegisterObjectResponse
 
 }
