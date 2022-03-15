d6f1b282f6c1e1abe3d65844d97e16ff9434df05
antoinedanibo <danibo.a@sfeir.com>
2022-03-15 15:46:00 +0100

    Added information in CredentialsObjectBody to normalize retrofit traffic

--- app/src/main/java/com/hyyu/votesimulation/network/body/CredentialsObjectBody.kt
+++ app/src/main/java/com/hyyu/votesimulation/network/body/CredentialsObjectBody.kt
@@ -1,7 +1,18 @@
 package com.hyyu.votesimulation.network.body
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
 data class CredentialsObjectBody (
+    @SerializedName("login")
+    @Expose
     var login: String,
+
+    @SerializedName("password")
+    @Expose
     var password: String,
+
+    @SerializedName("clientID")
+    @Expose
     var clientId: String
 )
