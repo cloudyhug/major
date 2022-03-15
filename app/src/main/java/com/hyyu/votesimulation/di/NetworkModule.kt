879fd6d56eada5b67f182d451de656d691bfbc23
antoinedanibo <danibo.a@sfeir.com>
2022-03-15 15:44:42 +0100

    Added a LoggingInterceptor

--- app/src/main/java/com/hyyu/votesimulation/di/NetworkModule.kt
+++ app/src/main/java/com/hyyu/votesimulation/di/NetworkModule.kt
@@ -9,8 +9,10 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -31,11 +33,12 @@ object NetworkModule {
     @Singleton
     @Provides
     fun provideRetrofit(sessionPrefs: Session, gson: Gson): Retrofit.Builder {
-        val clientBuilder = OkHttpClient.Builder()
-            .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
-            .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
-            .readTimeout(TIMEOUT, TimeUnit.SECONDS)
-            .addNetworkInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        val clientBuilder = OkHttpClient.Builder().apply {
+            connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+            writeTimeout(TIMEOUT, TimeUnit.SECONDS)
+            readTimeout(TIMEOUT, TimeUnit.SECONDS)
+            addNetworkInterceptor {
                 var request = it.request()
                 val newBuilder = request.newBuilder()
 
@@ -43,10 +46,14 @@ object NetworkModule {
                 request = newBuilder.build()
                 it.proceed(request)
             }
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            addInterceptor(loggingInterceptor)
+        }
 
         return Retrofit.Builder()
             .baseUrl(MajorApi.BASE_URL)
             .client(clientBuilder.build())
+            .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create(gson))
     }
 
