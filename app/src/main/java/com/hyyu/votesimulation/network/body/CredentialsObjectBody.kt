20a2edcea3614665e97dfaecde943f5f37cbaf48
antoinedanibo <danibo.a@sfeir.com>
2022-03-15 14:18:30 +0100

    Changed the CredentialsObjectBody data class

--- app/src/main/java/com/hyyu/votesimulation/network/body/CredentialsObjectBody.kt
+++ app/src/main/java/com/hyyu/votesimulation/network/body/CredentialsObjectBody.kt
@@ -2,5 +2,6 @@ package com.hyyu.votesimulation.network.body
 
 data class CredentialsObjectBody (
     var login: String,
-    var password: String
+    var password: String,
+    var clientId: String
 )
