7750c9d9ad549ac558b482835e9cd74afb11ae33
antoinedanibo <danibo.a@sfeir.com>
2022-03-25 18:04:09 +0100

    Fixing ViewModel, its content was not merged correctly

--- app/src/main/java/com/hyyu/votesimulation/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/hyyu/votesimulation/ui/main/viewmodel/MainViewModel.kt
@@ -1,16 +1,64 @@
 package com.hyyu.votesimulation.ui.main.viewmodel
 
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import android.util.Log
+import androidx.lifecycle.*
 import com.hyyu.votesimulation.repository.MainRepository
+import com.hyyu.votesimulation.util.state.DataState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel
-    @Inject constructor(
-        private val mainRepository: MainRepository,
-        private val savedStateHandle: SavedStateHandle
-    ): ViewModel() {
+@Inject constructor(
+    private val mainRepository: MainRepository,
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    companion object {
+        val TAG: String = MainViewModel::class.java.simpleName
+    }
+
+    private val _initDataState: MutableLiveData<DataState<Unit>> = MutableLiveData()
+    val initDataState: LiveData<DataState<Unit>>
+        get() = _initDataState
+
+    private var _electionsList = arrayListOf(
+        "Election 0",
+        "Election 1",
+        "Election 2",
+        "Election 3",
+        "Election 4",
+        "Election 5"
+    )
+
+    val electionsList: List<String>
+        get() = _electionsList
+
+    fun launchInitialCalls() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                Log.v(TAG, "Start Mock")
+                mockRefreshTokenCall()
+/*
+                // TODO: Uncomment once getting the new server executor
+                mainRepository.refreshAccessToken()
+                    .onEach {
+                        _initDataState.postValue(it)
+                    }
+                    .launchIn(viewModelScope)
+*/
+            }
+        }
+    }
+
+    // TODO: Remove once getting the new server executor
+    private suspend fun mockRefreshTokenCall() {
+        delay(1000)
+        _initDataState.postValue(DataState.Success(Unit))
+    }
 
 }
