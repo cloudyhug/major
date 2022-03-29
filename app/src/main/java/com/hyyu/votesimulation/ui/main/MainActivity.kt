fc01c8c4d65d06815c43dabf1665604e520f4b65
antoinedanibo <danibo.a@sfeir.com>
2022-03-29 15:44:02 +0200

    Implemented the UI so the result from /elections is showed in MainActivity in a list

--- app/src/main/java/com/hyyu/votesimulation/ui/main/MainActivity.kt
+++ app/src/main/java/com/hyyu/votesimulation/ui/main/MainActivity.kt
@@ -1,23 +1,25 @@
 package com.hyyu.votesimulation.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.Composable
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.hyyu.votesimulation.model.Election
 import com.hyyu.votesimulation.ui.main.state.MainStateEvent
 import com.hyyu.votesimulation.ui.main.viewmodel.MainViewModel
 import com.hyyu.votesimulation.ui.theme.MajorTheme
+import com.hyyu.votesimulation.util.state.DataState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -31,23 +33,38 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel.setStateEvent(MainStateEvent.InitialNetworkCalls)
-
-        setupObservers()
-    }
-
-    private fun initActivity() {
         setContent {
             MajorTheme {
                 ActivityScaffold()
             }
         }
+
+        setupObservers()
+
+        viewModel.setStateEvent(MainStateEvent.InitialNetworkCalls)
+    }
+
+    private fun initActivity() {
     }
 
     private fun setupObservers() {
-        viewModel.initDataState.observe(this) {
-            initActivity()
+        viewModel.initDataState.observe(this) { dataState ->
+            when (dataState) {
+                is DataState.Success -> initActivity()
+                // is DataState.Error -> DisplayErrorOnSnackbar(dataState.exception.message ?: "Unknown message")
+            }
         }
+    }
+
+    @Composable
+    private fun DisplayErrorOnSnackbar(message: String) {
+        val scope = rememberCoroutineScope()
+        val snackbarHostState = remember { SnackbarHostState() }
+
+        LaunchedEffect(snackbarHostState) {
+            snackbarHostState.showSnackbar(message)
+        }
+        SnackbarHost(snackbarHostState)
     }
 
     @Composable
@@ -60,11 +77,13 @@ class MainActivity : AppCompatActivity() {
             }
         },
         content = {
+            val itemList: List<Election> by viewModel.electionsList.observeAsState(listOf())
+
             Text(text = "Your history", fontSize = 16.sp, fontWeight = FontWeight.W700)
             Surface(color = MaterialTheme.colors.background) {
                 LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-                    items(items = viewModel.electionsList) { election ->
-                        ElectionCard(name = election)
+                    items(items = itemList) { election ->
+                        ElectionItem(name = election.name)
                     }
                 }
             }
@@ -72,7 +91,7 @@ class MainActivity : AppCompatActivity() {
     )
 
     @Composable
-    fun ElectionCard(name: String) {
+    fun ElectionItem(name: String) {
         Surface(
             modifier = Modifier.padding(16.dp),
             color = MaterialTheme.colors.primary
--- app/src/main/java/com/hyyu/votesimulation/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/hyyu/votesimulation/ui/main/viewmodel/MainViewModel.kt
@@ -1,13 +1,15 @@
 package com.hyyu.votesimulation.ui.main.viewmodel
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.*
+import com.hyyu.votesimulation.model.Election
 import com.hyyu.votesimulation.repository.MainRepository
 import com.hyyu.votesimulation.ui.main.state.MainStateEvent
 import com.hyyu.votesimulation.util.state.DataState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -29,16 +31,8 @@ class MainViewModel
     val initDataState: LiveData<DataState<Unit>>
         get() = _initDataState
 
-    private var _electionsList = arrayListOf(
-        "Election 0",
-        "Election 1",
-        "Election 2",
-        "Election 3",
-        "Election 4",
-        "Election 5"
-    )
-
-    val electionsList: List<String>
+    private var _electionsList: MutableLiveData<List<Election>> = MutableLiveData(listOf())
+    val electionsList: LiveData<List<Election>>
         get() = _electionsList
 
     fun setStateEvent(event: MainStateEvent) {
@@ -52,8 +46,20 @@ class MainViewModel
     private suspend fun launchInitialCalls() {
         mainRepository.refreshAccessToken()
             .onEach {
+                Log.v(TAG, "DataState from /refreshAccessToken: $it")
+                mainRepository.getElections()
+                    .onEach { electionsDataState ->
+                        Log.v(TAG, "DataState from /elections: $electionsDataState")
 
-                _initDataState.postValue(it)
+                        when (electionsDataState) {
+                            is DataState.Success -> _electionsList.postValue(electionsDataState.data as List<Election>)
+                            is DataState.Error -> _initDataState.postValue(DataState.Error(electionsDataState.exception))
+                            is DataState.Loading -> _initDataState.postValue(DataState.Loading)
+                        }
+
+                        _initDataState.postValue(DataState.Success(Unit))
+                    }
+                    .launchIn(viewModelScope)
             }
             .launchIn(viewModelScope)
     }
