df8ba26977028b96414524d08013e969fde16c8e
antoinedanibo <danibo.a@sfeir.com>
2022-09-01 18:11:35 +0200

    Implemented register and login requests

--- app/src/main/java/com/hyyu/votesimulation/repository/MainRepository.kt
+++ app/src/main/java/com/hyyu/votesimulation/repository/MainRepository.kt
@@ -6,6 +6,7 @@ import com.hyyu.votesimulation.network.MajorApi
 import com.hyyu.votesimulation.network.body.CredentialsObjectBody
 import com.hyyu.votesimulation.network.response.ConnectionObjectResponse
 import com.hyyu.votesimulation.prefs.Session
+import com.hyyu.votesimulation.util.const.CommonConst.LOADING_MINIMUM_DELAY
 import com.hyyu.votesimulation.util.state.DataState
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -30,34 +31,37 @@ constructor(
 
     init {
         if (sessionPrefs.deviceName.isNullOrEmpty()) {
-            sessionPrefs.deviceName = "${Build.MANUFACTURER}-${Build.MODEL} ${System.currentTimeMillis()}"
+            sessionPrefs.deviceName =
+                "${Build.MANUFACTURER}-${Build.MODEL} ${System.currentTimeMillis()}"
         }
     }
 
-    suspend fun registerNewUserAccount(body: CredentialsObjectBody): Flow<DataState<Unit>> = flow {
-        emit(DataState.Loading)
-        delay(1000)
-        try {
-            body.clientId = sessionPrefs.deviceName!!
-            Log.v(TAG, "${MajorApi.BASE_URL}${MajorApi.REGISTER}: body: $body")
-            majorApi.register(body)
-                .apply {
-                    if (isSuccessful) emit(DataState.Success(Unit))
-                    else throw Exception(headers()["message"])
-                }
-        } catch (e: Exception) {
-            Log.e(TAG, "error: ${e.message}")
-            emit(DataState.Error(e))
+    suspend fun registerNewUserAccount(body: CredentialsObjectBody): Flow<DataState<Unit>> =
+        flow {
+            emit(DataState.Loading)
+            delay(LOADING_MINIMUM_DELAY)
+            try {
+                body.clientId = sessionPrefs.deviceName!!
+                Log.v(TAG, "${MajorApi.BASE_URL}${MajorApi.REGISTER}: body: $body")
+                majorApi.register(body)
+                    .apply {
+                        if (isSuccessful) emit(DataState.Success(Unit))
+                        else throw Exception(headers()["message"])
+                    }
+            } catch (e: Exception) {
+                Log.e(TAG, "error: ${e.message}")
+                emit(DataState.Error(e))
+            }
         }
-    }
 
-    suspend fun logInToUserAccount(body: CredentialsObjectBody): Flow<DataState<ConnectionObjectResponse>> = flow {
-        emit(DataState.Loading)
-        delay(1000)
-        try {
-            body.clientId = sessionPrefs.deviceName!!
-            Log.v(TAG, "${MajorApi.BASE_URL}${MajorApi.CONNECT}: body: $body")
-            majorApi.connect(body)
+    suspend fun logInToUserAccount(body: CredentialsObjectBody): Flow<DataState<ConnectionObjectResponse>> =
+        flow {
+            emit(DataState.Loading)
+            delay(LOADING_MINIMUM_DELAY)
+            try {
+                body.clientId = sessionPrefs.deviceName!!
+                Log.v(TAG, "${MajorApi.BASE_URL}${MajorApi.CONNECT}: body: $body")
+                majorApi.connect(body)
                     .apply {
                         if (isSuccessful) {
                             body()?.let {
@@ -67,11 +71,11 @@ constructor(
                             } ?: throw Exception("Request body was null")
                         } else throw Exception(headers()["message"])
                     }
-        } catch (e: Exception) {
-            Log.e(TAG, "error: ${e.message}")
-            emit(DataState.Error(e))
+            } catch (e: Exception) {
+                Log.e(TAG, "error: ${e.message}")
+                emit(DataState.Error(e))
+            }
         }
-    }
 
     suspend fun refreshAccessToken(): Flow<DataState<Unit>> = flow {
         try {
@@ -83,8 +87,7 @@ constructor(
                                 sessionPrefs.accessToken = token
                                 emit(DataState.Success(Unit))
                             } ?: throw Exception("Request body was null")
-                        }
-                        else throw Exception(headers()["message"])
+                        } else throw Exception(headers()["message"])
                     }
             }
         } catch (e: Exception) {
--- app/src/main/java/com/hyyu/votesimulation/ui/launcher/LauncherViewModel.kt
+++ app/src/main/java/com/hyyu/votesimulation/ui/launcher/LauncherViewModel.kt
@@ -1,18 +1,25 @@
 package com.hyyu.votesimulation.ui.launcher
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hyyu.votesimulation.navigation.BackCommand
 import com.hyyu.votesimulation.navigation.directions.launcher.LauncherNavigationDirections
 import com.hyyu.votesimulation.navigation.navigators.AppNavigator
+import com.hyyu.votesimulation.network.body.CredentialsObjectBody
+import com.hyyu.votesimulation.repository.MainRepository
 import com.hyyu.votesimulation.ui.launcher.event.LauncherEvent
-import com.hyyu.votesimulation.ui.launcher.event.LauncherEvent.GoToSignup
-import com.hyyu.votesimulation.ui.launcher.event.LauncherEvent.Login
-import com.hyyu.votesimulation.ui.launcher.event.LauncherEvent.Signup
+import com.hyyu.votesimulation.ui.launcher.event.LauncherEvent.*
+import com.hyyu.votesimulation.ui.launcher.state.LauncherState
+import com.hyyu.votesimulation.util.state.DataState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LauncherViewModel @Inject constructor(
+    private val mainRepository: MainRepository,
     private val appNavigator: AppNavigator
 ) : ViewModel() {
 
@@ -20,12 +27,20 @@ class LauncherViewModel @Inject constructor(
         val TAG: String = LauncherViewModel::class.java.simpleName
     }
 
+    private val _uiState = MutableStateFlow(LauncherState())
+    val uiState: StateFlow<LauncherState> = _uiState
+
     fun handleEvent(event: LauncherEvent) {
         when (event) {
+            is Back -> navigateBack()
             is GoToSignup -> navigateToSignup()
             is Signup -> registerAccount(event)
             is Login -> logInToAccount(event)
         }
+    }
+
+    private fun navigateBack() {
+        appNavigator.navigate(BackCommand)
     }
 
     private fun navigateToSignup() {
@@ -33,11 +48,35 @@ class LauncherViewModel @Inject constructor(
     }
 
     private fun registerAccount(event: Signup) {
+        val credentials = CredentialsObjectBody(event.login, event.password, "")
+        viewModelScope.launch {
+            mainRepository.registerNewUserAccount(credentials)
+                .flowOn(Dispatchers.IO)
+                .onEach { dataState ->
+                    _uiState.value = _uiState.value.build {
+                        isLoading = dataState is DataState.Loading
+                        isFailure = dataState is DataState.Error
+                        if (dataState is DataState.Success)
+                            this.credentials = credentials
+                    }
+                }.launchIn(this)
+        }
     }
 
     private fun logInToAccount(event: Login) {
-        Log.v("$TAG/Login", "login: ${event.login}")
-        Log.v("$TAG/Login", "password: ${event.password}")
+        val credentials = CredentialsObjectBody(event.login, event.password, "")
+
+        viewModelScope.launch {
+            mainRepository.logInToUserAccount(credentials)
+                .flowOn(Dispatchers.IO)
+                .onEach { dataState ->
+                    _uiState.value = _uiState.value.build {
+                        isLoading = dataState is DataState.Loading
+                        isFailure = dataState is DataState.Error
+                        isAuthenticated = dataState is DataState.Success
+                    }
+                }.launchIn(this)
+        }
     }
 
 }
