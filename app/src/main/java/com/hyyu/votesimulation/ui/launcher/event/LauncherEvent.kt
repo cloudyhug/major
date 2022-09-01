cd3ef999f9ba54914a37584fb9e89bdf7d648abb
antoinedanibo <danibo.a@sfeir.com>
2022-09-01 18:11:15 +0200

    New event for Launcher

--- app/src/main/java/com/hyyu/votesimulation/ui/launcher/event/LauncherEvent.kt
+++ app/src/main/java/com/hyyu/votesimulation/ui/launcher/event/LauncherEvent.kt
@@ -1,6 +1,7 @@
 package com.hyyu.votesimulation.ui.launcher.event
 
 sealed class LauncherEvent {
+    object Back : LauncherEvent()
     object GoToSignup : LauncherEvent()
     data class Signup(val login: String, val password: String): LauncherEvent()
     data class Login(val login: String, val password: String): LauncherEvent()
