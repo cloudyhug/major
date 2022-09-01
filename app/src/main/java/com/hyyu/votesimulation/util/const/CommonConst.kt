02d72e86af31e47c86dd55fe6c76d58de8a82874
antoinedanibo <danibo.a@sfeir.com>
2022-09-01 18:12:19 +0200

    Added a constant

--- app/src/main/java/com/hyyu/votesimulation/util/const/CommonConst.kt
+++ app/src/main/java/com/hyyu/votesimulation/util/const/CommonConst.kt
@@ -3,4 +3,5 @@ package com.hyyu.votesimulation.util.const
 object CommonConst {
     const val EMPTY_STRING = ""
     const val RETROFIT_TIMEOUT = 10L
+    const val LOADING_MINIMUM_DELAY = 1000L
 }
