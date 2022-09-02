8532f0e4851112db8d63fabe48437e95981a1531
antoinedanibo <danibo.a@sfeir.com>
2022-09-02 11:45:37 +0200

    Base ViewModel template

---
+++ app/src/main/java/com/hyyu/votesimulation/util/base/BaseViewModel.kt
@@ -0,0 +1,22 @@
+package com.hyyu.votesimulation.util.base
+
+import androidx.lifecycle.ViewModel
+import com.hyyu.votesimulation.model.common.snackbar.MajorSnackbarData
+import com.hyyu.votesimulation.model.common.snackbar.SnackbarType
+import com.hyyu.votesimulation.model.common.snackbar.SnackbarType.ERROR
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+open class BaseViewModel : ViewModel() {
+    private val _snackbarState = MutableStateFlow<MajorSnackbarData?>(null)
+    val snackbarState: StateFlow<MajorSnackbarData?> = _snackbarState
+
+    protected fun showSnackBar(message: String, type: SnackbarType = ERROR) {
+        _snackbarState.value = MajorSnackbarData(message, type)
+    }
+
+    fun onSnackbarDisplayed() {
+        _snackbarState.value = null
+    }
+
+}
