f0893f68feb2cebfa15f5dcdc4b91f9ebcd97c2d
danibo.a <danibo.a@sfeir.com>
2022-09-13 18:07:54 +0200

    Added strings

--- app/src/main/java/com/hyyu/votesimulation/util/const/CommonConst.kt
+++ app/src/main/java/com/hyyu/votesimulation/util/const/CommonConst.kt
@@ -1,7 +1,15 @@
 package com.hyyu.votesimulation.util.const
 
+import com.hyyu.votesimulation.network.MajorApi
+
 object CommonConst {
     const val EMPTY_STRING = ""
+
     const val RETROFIT_TIMEOUT = 10L
     const val LOADING_MINIMUM_DELAY = 1000L
+    const val RETROFIT_REQUEST_TIMEOUT_MESSAGE = "Failed to connect to ${MajorApi.IP_ADDRESS}"
+
+    const val REGISTER_ERROR_CREDENTIALS = "UnacceptableCredentials"
+    const val REGISTER_ERROR_USER_ALREADY_REGISTERED = "UserAlreadyRegistered"
+
 }
